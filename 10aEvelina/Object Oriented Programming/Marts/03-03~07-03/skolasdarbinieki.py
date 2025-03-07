9e07c8d9e7497a525a349f90398f14693b1318a1
Evelina Marta Druvnese <evelina.druvnese@svg.lv>
2025-03-07 08:14:39 +0200

    Folder changes

--- 10aEvelina/Object Oriented Programming/Marts/03-03~07-03/skolasdarbinieki.py
+++ 10aEvelina/Object Oriented Programming/Marts/03-03~07-03/skolasdarbinieki.py
@@ -11,21 +11,21 @@ class SkolasDarbinieks(ABC):
     def apraksts(self):
         pass
 
-class Skolotajs(SkolasDarbinieks):
+class Skolotajs(SkolasDarbinieks):   #skolotāja apakšklase, kas pieprasa priekšmetu
     def apraksts(self):
         return f"{self.vards} {self.uzvards} māca priekšmetu '{self.prieksmets}'."
 
-class Dezurants(SkolasDarbinieks):
+class Dezurants(SkolasDarbinieks):    #dežuranta apakšklase, kas nepieprasa priekšmetu
     def apraksts(self):
         return f"{self.vards} {self.uzvards} saka, ka jāsavāc pazaudētās mantas no skolas pirmā stāva!"
 
-class Saimnieks(SkolasDarbinieks):
+class Saimnieks(SkolasDarbinieks):    #saimnieka apakšklase
     def apraksts(self):
         return f"{self.vards} {self.uzvards} labo salauzto loga eņģi."
 
-darbinieki = [Skolotajs("Anda", "Lāce", "Audzināšana"), Dezurants("Jānis", "Straume"), Saimnieks("Johans", "Kalniņš")]
+darbinieki = [Skolotajs("Anda", "Lāce", "Audzināšana"), Dezurants("Jānis", "Straume"), Saimnieks("Johans", "Kalniņš")]      #saraksts ar skolas darbinieku objektiem un tiem atbilstošajiem datiem
 
-for darbinieks in darbinieki:
+for darbinieks in darbinieki:        #cikls kas, ejot cauri darbinieku sarakstam gan tos parāda konsolē, gan saglabā failā
     print(darbinieks.apraksts())
     with open('darbinieki.txt', 'a', encoding='utf-8') as fails:
         fails.write(f'{darbinieks.apraksts()}\n')
