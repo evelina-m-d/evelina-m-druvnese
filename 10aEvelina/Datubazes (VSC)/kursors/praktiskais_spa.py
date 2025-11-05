a8dbf867304ceb18dbb633e2b15ef163e5080487
evelina-m-d <144446944+evelina-m-d@users.noreply.github.com>
2025-11-05 14:24:02 +0200

    kursors

--- 10aEvelina/Datubazes (VSC)/kursors/praktiskais_spa.py
+++ 10aEvelina/Datubazes (VSC)/kursors/praktiskais_spa.py
@@ -2,10 +2,6 @@ import sqlite3
 savienojums=sqlite3.connect("spa_sistema.db")
 cursor=savienojums.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS klienti")
-cursor.execute("DROP TABLE IF EXISTS pakalpojumi")
-cursor.execute("DROP TABLE IF EXISTS pieraksti")
-
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS klienti (
         klienti_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -29,9 +25,44 @@ cursor.execute("""
         pieraksta_id INTEGER PRIMARY KEY AUTOINCREMENT,
         klients TEXT NOT NULL,
         pakalpojums TEXT NOT NULL,
-        datums TEXT NOT NULL
+        datums TEXT NOT NULL,
+        FOREIGN KEY (klients) REFERENCES klienti(klienti_id),
+        FOREIGN KEY (pakalpojums) REFERENCES pakalpojumi(pakalpojuma_id)
     )
 """)
 
-print("Datubāze un tabulas izveidotas!")
+print("--Pievienot klientu")
+vards=input("Ievadi vardu:")
+uzvards=input("Ievadi uzvardu")
+tel_numurs=input("Ievadi telefona nr:")
 
+cursor.execute("""
+    INSERT INTO klienti(vards,uzvards,tel_numurs) VALUES(?,?,?)""",
+    (vards, uzvards, tel_numurs)
+)
+savienojums.commit()
+print("Klients pievienots!")
+
+print("--pievienot pakalpojumu")
+nosaukums=input("Ievadi pakalpojuma nosaukumu:")
+cena=input("Ievadi pakalpojuma cenu:")
+ilgums=input("Ievadi pakalpojuma ilgumu:")
+
+cursor.execute("""
+    INSERT INTO pakalpojumi(nosaukums,cena,ilgums_minutes) VALUES(?,?,?)""",
+    (nosaukums, cena, ilgums)
+)
+savienojums.commit()
+
+print("Pakalpojums pievienots!")
+
+print("Pieraksta pievienošana -")
+klients=input("Klienta id:")
+pakalpojums=input("Pakalpojuma id:")
+datums=input("Datums:")
+
+cursor.execute("""
+    INSERT INTO pieraksti(klients,pakalpojums,datums) VALUES(?,?,?)""",
+    (klients, pakalpojums, datums)
+)
+savienojums.commit()
