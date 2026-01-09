17728c7ed65bc32a269326d44c77fe10057a734a
evelina-m-d <144446944+evelina-m-d@users.noreply.github.com>
2026-01-09 11:44:38 +0200

    pabeigta pirma ui programma

--- 12aEvelina/Datubazes (VSC)/ui/datu_iev_db.py
+++ 12aEvelina/Datubazes (VSC)/ui/datu_iev_db.py
@@ -77,6 +77,63 @@ def main(page: ft.Page):
     statuss2=ft.Text("")
     poga_dalibnieki=ft.ElevatedButton("Saglabāt datus", on_click=saglabat_dalibniekus)
 
+    dalibu_teksts=ft.Text(value="Jaunas dalības ievade!")
+    
+    dd_dalibnieks=ft.Dropdown(label="Izvēlies dalībnieku")
+    dd_sovs=ft.Dropdown(label="Izvēlies šovu")
+    dd_loma=ft.TextField(label="Loma:")
+
+    def ieladet_dropdown():
+        cursor.execute("""
+        SELECT * FROM sovu_dalibnieki
+        ORDER BY uzvards, vards
+        """)
+        dal=cursor.fetchall()
+        for r in dal:
+            dd_dalibnieks.options.append(ft.dropdown.Option(key=str(r[0]),text=f"{r[1]} {r[2]}, {r[3]}"))
+        
+        cursor.execute("""
+        SELECT * FROM televizijas_sovi
+        ORDER BY nosaukums
+        """)
+        sovi=cursor.fetchall()
+        dd_sovs.options=[
+            ft.dropdown.Option(
+                key=str(r[0]),
+                text=f"{r[1]}, {r[2]}, {r[4]}"
+            )
+            for r in sovi
+        ]
+        page.update()
+
+    ieladet_dropdown()
+
+    def saglabat_dalibu(_):
+        if dd_dalibnieks.value is None or dd_sovs.value is None or dd_sovs.value=="":
+            statuss3.value="Kļuda! Aizpildi visus laukus!"
+            statuss3.color="red"
+        else:
+            dal_id = dd_dalibnieks.value.strip()
+            dal_sovs = dd_sovs.value.strip()
+            dal_loma = dd_sovs.value.strip()
+
+            cursor.execute("""
+            INSERT INTO sovu_dalibas(sovu_dalibnieki_id, televizijas_sovi_id, loma)
+            VALUES(?,?,?)
+            """,
+            (int(dal_id), int(dal_sovs), dal_loma)
+            )
+            savienojums.commit()
+            statuss3.value="Dati pievienoti!"
+            statuss3.color="green"
+
+            dd_dalibnieks.value=""
+            dd_sovs.value=""
+            dd_loma.value=""
+
+    poga_dalibas=ft.ElevatedButton("Saglabāt datus", on_click=saglabat_dalibu)
+    statuss3=ft.Text(value="")
+
     page.add(
         sovu_teksts,
         nosaukums, 
@@ -91,6 +148,13 @@ def main(page: ft.Page):
         uzvards,
         profesija,
         statuss2,
-        poga_dalibnieki)
+        poga_dalibnieki,
+        
+        dalibu_teksts,
+        dd_dalibnieks,
+        dd_sovs,
+        dd_loma,
+        statuss3,
+        poga_dalibas)
 
 ft.run(main)
